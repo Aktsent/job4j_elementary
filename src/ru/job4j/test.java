ec56b8f23286d68a32c24e9c95c30466d1448d7a
Aktsent <accent43@yandex.ru>
2021-07-22 12:43:36 +0300

    ru.job4j.calculator.ArgMethod

---
+++ src/ru/job4j/test.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class test {
+        public static void plus(int first, int second) {
+
+            int result = first + second;
+
+            System.out.println(result);}
+
+        public static void main(String [ ] args) {
+
+                test.plus(100, 500);
+
+                test.plus(4, 2);
+
+                test.plus(3, 5);}}
+
