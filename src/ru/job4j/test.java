57082ad9526282858a3af7d3654f1a1fbe8ca7e1
Aktsent <accent43@yandex.ru>
2021-07-23 12:18:15 +0300

    ru.job4j.calculator.Fit

--- src/ru/job4j/test.java
+++ src/ru/job4j/test.java
@@ -1,17 +1,12 @@
 package ru.job4j;
 
 public class test {
-        public static void plus(int first, int second) {
 
-            int result = first + second;
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+        test.plus(1, 2);
+        test.plus(10, 11);
+    }
 
-            System.out.println(result);}
-
-        public static void main(String [ ] args) {
-
-                test.plus(100, 500);
-
-                test.plus(4, 2);
-
-                test.plus(3, 5);}}
-
+}
