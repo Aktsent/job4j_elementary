a4d3fd66b1fec20035aa5b0429ef051acc9f29e5
Aktsent <accent43@yandex.ru>
2021-07-27 11:53:20 +0300

    ru.job4j.condition.Greeting

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,13 +1,13 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value){
+    public static float rubleToEuro(float value) {
         return value / 70;
     }
-    public static float rubleToDollar(float value){
+    public static float rubleToDollar(float value) {
         return value / 60;
     }
-    public static void main(String [] args){
+    public static void main(String [] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
