50cef4ea4d3639bd2c1fb7fbacf1468ff23f2b81
Aktsent <accent43@yandex.ru>
2021-07-28 14:11:39 +0300

    ru.job4j.converter.Converter

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -9,12 +9,14 @@ public class Converter {
     }
     public static void main(String [] args) {
         float in = 140;
-        float expected =2;
+        float expected = 2;
         float out = Converter.rubleToEuro(in);
-        float out2 = Converter.rubleToDollar(in);
         boolean passed = expected == out;
-        boolean passed2 = expected == out2;
         System.out.println("140 rubles are 2. Test result : " + passed);
-        System.out.println("140 rubles are 2. Test result : " + passed2);
+        float in1 = 140;
+        float expected1 = 2;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2. Test result : " + passed1);
     }
 }
