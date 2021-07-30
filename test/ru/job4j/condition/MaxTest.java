8cf9c3ed2aaba67a2043c89304d9dcfaef2d7822
Aktsent <accent43@yandex.ru>
2021-07-30 16:45:53 +0300

    Max

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+       int left = 1;
+       int right = 2;
+       int result = Max.max(left, right);
+       int expected = 2;
+       Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenLeft6Right4Then6() {
+       int left = 6;
+       int right = 4;
+       int expected = 6;
+       int out = Max.max(left, right);
+       Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenLeft10Right7Then7() {
+        int left = 10;
+        int right = 7;
+        int expected = 10;
+        int out = Max.max(left, right);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenLeft6Right6Then6() {
+        int left = 6;
+        int right = 6;
+        int expected = 6;
+        int out = Max.max(left, right);
+        Assert.assertEquals(expected, out);
+    }
+}
