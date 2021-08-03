8268f979b3d2266d38150cfa6f35fb550ed36415
Aktsent <accent43@yandex.ru>
2021-08-03 13:29:38 +0300

    LogicNot

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -17,9 +17,9 @@ public class TriangleTest {
     }
     @Test
     public void whenExists() {
-        double ab = 2.0;
-        double ac = 2.0;
-        double bc = 2.0;
+        double ab = 1.0;
+        double ac = 1.0;
+        double bc = 5.0;
         boolean result = Triangle.exist(ab, ac, bc);
         Assert.assertFalse(result);
     }
