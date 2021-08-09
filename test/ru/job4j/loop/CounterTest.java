a6893b10a9e8ba9ed7e5e1663d738ef4617addf1
Aktsent <accent43@yandex.ru>
2021-08-09 17:39:01 +0300

    Counter

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int expected = 30;
+        int result = Counter.sumByEven(start, finish);
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenSumEvenNumbers() {
+        int start = 2;
+        int finish = 14;
+        int expected = 56;
+        int result = Counter.sumByEven(start, finish);
+        Assert.assertEquals(expected, result);
+    }
+}
