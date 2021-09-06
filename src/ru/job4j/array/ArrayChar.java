05560d0b8a583a972175db6b58a0dc4c9c95b63c
Aktsent <accent43@yandex.ru>
2021-09-06 11:30:29 +0300

    ArrayChar

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,12 +2,10 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        for (char i = 0; i < word.length -1; i++) {
-            for (char i1 = 0; i1 < pref.length -1; i1++) {
-                if (word[1] != pref[1]) {
+        for (int i = 0; i < pref.length; i++) {
+                if (word[i] != pref[i]) {
                     return false;
                 }
-            }
         }
         return true;
     }
