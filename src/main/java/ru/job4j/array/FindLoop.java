e13211973847bdba9bb469921c084a029c352608
Aktsent <accent43@yandex.ru>
2021-09-13 12:45:01 +0300

    FindLoop

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -13,8 +13,8 @@ public class FindLoop {
     }
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rsl = -1;
-        for (int i = start; i < finish; i++) {
-            if ((data[i]) == el) {
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
                 rsl = i;
             }
         }
