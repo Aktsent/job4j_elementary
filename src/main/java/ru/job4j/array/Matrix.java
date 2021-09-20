41991976a5c399ebed64131555dbb787f9e5df8c
Aktsent <accent43@yandex.ru>
2021-09-20 16:04:02 +0300

    Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -2,9 +2,9 @@ package ru.job4j.array;
 
 public class Matrix {
     public static int[][] multiple(int size) {
-        int[][] array = {{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
-        for (int i = size; i < array.length; i++) {
-            for (int j = size; j < array[i].length; j++) {
+        int[][] array = new int[size][size];
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 1; j < array[i].length; j++) {
                 array[i][j] = i * j;
             }
         }
