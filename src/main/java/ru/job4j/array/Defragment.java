25f1fcf31caae19e35f072fa9faa0a8665718500
Aktsent <accent43@yandex.ru>
2021-09-23 12:54:02 +0300

    Defragment

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == null && array[j] != null) {
+                    Defragment.swap(array, i, j);
+                }
+            }
+            }
+        return array;
+    }
+
+    public static String[] swap(String[] array, int point, int notnull) {
+        String temp = array[point];
+        array[point] = array[notnull];
+        array[notnull] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int i = 0; i < compressed.length; i++) {
+            System.out.print(compressed[i] + " ");
+        }
+    }
+}
