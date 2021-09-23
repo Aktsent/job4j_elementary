d472d385d2fbba930bedc9cde99d103f25381467
Aktsent <accent43@yandex.ru>
2021-09-23 15:58:35 +0300

    Defragment

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class Defragment {
     public static String[] compress(String[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+            for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == null && array[j] != null) {
                     Defragment.swap(array, i, j);
                 }
