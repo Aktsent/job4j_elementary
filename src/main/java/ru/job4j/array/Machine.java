bc3a9fb77297531ef62a8c87b2ab5a0af647ca8f
Aktsent <accent43@yandex.ru>
2021-09-28 10:54:00 +0300

    Machine

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,25 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (change >= 10) {
-                change = change - 10;
-                rsl = coins;
-                size++;
-            }
-            while (change >= 5) {
-                change = change - 5;
-                rsl = coins;
-                size++;
-            }
-            while (change >= 2) {
-                change = change - 2;
-                rsl = coins;
-                size++;
-            }
-            while (change >= 1) {
-                change = change - 1;
-                rsl = coins;
+        for (int i : coins) {
+            while (change >= coins[i]) {
+                change = change - coins[i];
+                rsl[size] = coins[i];
                 size++;
             }
         }
